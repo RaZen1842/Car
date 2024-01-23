ff7f728c47e9b3f69d0eabf330735dd1022d6ef7
Shreyas Veturi <veturi.s@etoncollege.org.uk>
2024-01-23 14:57:43 +0000

    Initial Commit

---
+++ carCustomiser/carCustomiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  carCustomiserApp.swift
+//  carCustomiser
+//
+//  Created by Veturi, Shreyas (HJRM) on 23/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct carCustomiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
