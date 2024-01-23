6ade06742a3a90f323dd2ce7b7ef6def58ccbad3
Shreyas Veturi <veturi.s@etoncollege.org.uk>
2024-01-23 20:34:36 +0000

    Furthering the toggles

--- carCustomiser/ContentView.swift
+++ carCustomiser/ContentView.swift
@@ -8,15 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
-    let starterCars = StarterCars()
+    @State private var starterCars = StarterCars()
     @State private var selectedCar: Int = 0
     
+    @State private var exhaustPackage = false
+    @State private var tiresPackage = false
+    
     var body: some View {
-        VStack(alignment: .leading, spacing: 20) {
-            Text("\(starterCars.cars[selectedCar].displayStats())")
-            Button("Random Car", action: {
-                selectedCar = Int.random(in: 0..< self.starterCars.cars.count)
-            })
+        
+        let exhaustPackageBinding = Binding<Bool> (
+            get: { self.exhaustPackage },
+            set: { newValue in
+                self.exhaustPackage = newValue
+                if newValue == true {
+                    starterCars.cars[selectedCar].topSpeed += 5
+                }
+                else {
+                    starterCars.cars[selectedCar].topSpeed -= 5
+                }
+                
+            }
+        )
+        
+        let tiresPackageBinding = Binding<Bool> (
+            get: { self.tiresPackage },
+            set: { newValue in
+                self.tiresPackage = newValue
+                if newValue == true {
+                    starterCars.cars[selectedCar].handling += 1
+                }
+                else {
+                    starterCars.cars[selectedCar].handling -= 1
+                }
+                
+            }
+        )
+        
+        Form {
+            VStack(alignment: .leading, spacing: 20) {
+                Text("\(starterCars.cars[selectedCar].displayStats())")
+                Button("Next Car", action: {
+                    selectedCar += 1
+                    if selectedCar == self.starterCars.cars.count {
+                        selectedCar = 0
+                    }
+                })
+            }
+            Section {
+                Toggle("Exhaust Package", isOn: exhaustPackageBinding)
+                Toggle("Tires Package", isOn: tiresPackageBinding)
+            }
         }
     }
 }
--- carCustomiser/StarterCars.swift
+++ carCustomiser/StarterCars.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct StarterCars {
-    let cars = [Car(make: "Mazda", model: "MX-5", topSpeed: 125, acceleration: 7.7, handling: 5),
+    var cars = [Car(make: "Mazda", model: "MX-5", topSpeed: 125, acceleration: 7.7, handling: 5),
                 Car(make: "Toyota", model: "Supra", topSpeed: 140, acceleration: 4.8, handling: 7),
                 Car(make: "Ferrari", model: "F40", topSpeed: 229, acceleration: 3.1, handling: 6),
                 Car(make: "Porsche", model: "911", topSpeed: 182, acceleration: 4.2, handling: 9)]
